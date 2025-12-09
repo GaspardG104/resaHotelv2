c66982c8af2a13b55d695505fc20580788573d43
Gaspard <gaspardguidetti@gmail.com>
2025-12-09 15:43:37 +0100

    fin de journée; css et amélioration de chambre.ejs

--- apps.js
+++ apps.js
@@ -2,8 +2,10 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
+
 import chambreRoutes from './routes/chambres.js';
-//import clientRoutes from './routes/clients.js';
+import clientsRoutes from './routes/clients.js';
+
 
 const app = express();
 // Routes
@@ -12,18 +14,19 @@ const PORT = process.env.PORT || 3000;
 // Récupérer le chemin local (file://...) et le répertoire courant (workspace/resaHotelCalifornia2)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 // Configuration EJS
-// Dossier public pour les fichiers statiques (CSS, JS, images)
-app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+// Dossier public pour les fichiers statiques (CSS, JS, images)
+app.use(express.static('public'));
+
 // Middleware (ajout de la route /public, gestion JSON et URLencoded)
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/chambres', chambreRoutes);
-//app.use('/clients', clientRoutes);
 // Configuration du middleware avec exclusion
 app.use('/semantic-ui', express.static(
     path.join(__dirname, 'node_modules', 'semantic-ui-css'),
@@ -52,4 +55,8 @@ app.listen(PORT, () => {
 
 import clientRoutes from './routes/clients.js';
 
+
+app.use('/chambres', chambreRoutes);
 app.use('/clients', clientRoutes);
+
+
