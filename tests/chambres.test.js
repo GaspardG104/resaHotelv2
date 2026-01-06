d6a0a75bb9882ab657832433272ddb3406a8a140
Gaspard <gaspardguidetti@gmail.com>
2026-01-06 11:55:18 +0100

    ajout de vitest pour les test unitaires

--- tests/chambres.test.js
+++ tests/chambres.test.js
@@ -1,13 +1,13 @@
 import { describe, it, expect, vi } from 'vitest'
-import CtrlTodo from '../views/chambres/chambre.ejs'
+import chambres from '../views/chambres'
 
 describe("tests sur les chambres du crud", () => {
 
-    it('chambre doit ajouter une nouvelle chambre et répondre avec 201', () => {
+    it('chambre doit ajouter une nouvelle chambre et répondre avec 333', () => {
         // 1. On prépare une requête simulée avec un corps valide
         const req = {
             body: {
-                name: '101',
+                name: '333',
                 capacite: 2
             }
         }
@@ -19,15 +19,15 @@ describe("tests sur les chambres du crud", () => {
             json: vi.fn()
         }
         // 3. On appelle la fonction à tester
-        CtrlTodo.createChambre(req, res)
-        // 4. On vérifie que res.status a été appelé avec 201
-        expect(res.status).toHaveBeenCalledWith(201)
+        chambres.create(req, res)
+        // 4. On vérifie que res.status a été appelé avec 333
+        expect(res.status).toHaveBeenCalledWith(333)
         // 5. On vérifie que res.json a été appelé avec un objet contenant la nouvelle tâche
         expect(res.json).toHaveBeenCalledWith(
             expect.objectContaining({
                 success: true,
                 data: expect.objectContaining({
-                    name: '101',
+                    name: '333',
                     capacite: 2
                 }),
                 message: 'Chambre créée avec succès'
