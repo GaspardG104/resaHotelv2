9dea71b6869097615756a584687c36473def4852
Gaspard <gaspardguidetti@gmail.com>
2026-01-06 14:54:35 +0100

    ajouts de tests

--- tests/chambres.test.js
+++ tests/chambres.test.js
@@ -1,38 +1,38 @@
 import { describe, it, expect, vi } from 'vitest'
-import chambres from '../views/chambres'
+import ChambreModels from '../models/chambre.js'
 
 describe("tests sur les chambres du crud", () => {
 
     it('chambre doit ajouter une nouvelle chambre et répondre avec 333', () => {
         // 1. On prépare une requête simulée avec un corps valide
-        const req = {
-            body: {
-                name: '333',
-                capacite: 2
-            }
+        const data = {
+            numero: '333',
+            capacite: 5
+    
         }
-        // 2. On crée un objet res avec des fonctions mockées
-        // - res.status doit pouvoir être chaîné (retourne res)
-        // - res.json doit enregistrer les appels
-        const res = {
-            status: vi.fn().mockReturnThis(),
-            json: vi.fn()
-        }
-        // 3. On appelle la fonction à tester
-        chambres.create(req, res)
-        // 4. On vérifie que res.status a été appelé avec 333
-        expect(res.status).toHaveBeenCalledWith(333)
+
         // 5. On vérifie que res.json a été appelé avec un objet contenant la nouvelle tâche
-        expect(res.json).toHaveBeenCalledWith(
-            expect.objectContaining({
-                success: true,
-                data: expect.objectContaining({
-                    name: '333',
-                    capacite: 2
-                }),
-                message: 'Chambre créée avec succès'
-            })
-        )
+        expect(ChambreModels.create(data)).not.toBe(0)
 
     })
+
+    it('verifier que la chambre 333 existe', () => {
+                const data = {
+            numero: '333',
+            capacite: 5
+    
+        }
+
+        // 5. On vérifie que res.json a été appelé avec un objet contenant la nouvelle tâche
+        expect(ChambreModels.create(data)).toThrow()
+
+    })
+
+    it('verifie la capacité de la chambre 333', () => {
+
+
+
+    })
+
+
 })
