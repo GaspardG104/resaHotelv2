70abdf0394ace5b1fa387d6e04af6869e472a201
Gaspard <gaspardguidetti@gmail.com>
2026-01-15 16:31:19 +0100

    ajouts d'un meilleur css

--- tests/chambres.test.js
+++ tests/chambres.test.js
@@ -1,48 +1,62 @@
-import { describe, it, expect, vi } from 'vitest'
-import ChambreModels from '../models/chambre.js'
+import { describe, it, expect, vi } from 'vitest';
+import ChambreController from '../controllers/chambreController.js';
+import ChambreModels from '../models/chambre.js';
 
 describe("tests sur les chambres du crud", () => {
+    let req, res;
 
-    it('doit crrer une chambre avec un titre vide pour vérifié si le site bloque', () => {
-        const data = {
-            numero: '',
-            capacite: 1
-        }
+    beforeEach(() => {
+        vi.clearAllMocks();
+        req = { params: {}, body: {} };
+        res = {
+            render: vi.fn(),
+            redirect: vi.fn(),
+            status: vi.fn().mockReturnThis(),
+            send: vi.fn()
+        };
+    });
+    describe("tests sur la partie création de chambres"),() => {
+        describe("tests sur create(data)"),() => {
+            it('doit créer une chambre avec un titre vide pour vérifié si le site bloque bien la création', () => {
+                const data = {
+                    numero: '',
+                    capacite: 1
+                }
 
-        //expect(ChambreModels.create(data)).not.toBe()
-        expect(ChambreModels.create(data)).rejects.toBeTruthy()
-    })
+                //expect(ChambreModels.create(data)).not.toBe()
+                expect(ChambreModels.create(data)).rejects.toBeTruthy()
+            })
 
-    it('chambre doit ajouter une nouvelle chambre et répondre avec 333', () => {
-        // 1. On prépare une requête simulée avec un corps valide
-        const data = {
-            numero: '333',
-            capacite: 5
-    
-        }
+            it('chambre doit ajouter une nouvelle chambre et répondre avec 333', () => {
+                // 1. On prépare une requête simulée avec un corps valide
+                const data = {
+                    numero: '333',
+                    capacite: 5
+            
+                }
 
-        // 5. On vérifie que res.json a été appelé avec un objet contenant la nouvelle tâche
-        expect(ChambreModels.create(data)).not.Contain(data)
+                expect(ChambreModels.create(data)).toBe()
 
-    })
+            })
+        };
+    };
 /*
     it('verifier que la chambre 333 existe', () => {
             const data = {
             numero: '333',
-            capacite: 5
+            capacite: 3
     
         }
 
-        // 5. On vérifie que res.json a été appelé avec un objet contenant la nouvelle tâche
         expect(ChambreModels.index(data)).not.toBe(0)
 
     })
 
     // on verife que la chambre a bien été modifiée
-    it('verifie la modification du numéro et de la capacité de la chambre 333', () => {
+    it('verifie la modification du numéro et de la capacité de la chambre 333 -> 666', () => {
             const data = {
-            numero: '333',
-            capacite: 5
+            numero: '666',
+            capacite: 6
         }
 
         expect(ChambreModels.index(data)).not.toBe(0)
@@ -52,11 +66,11 @@ describe("tests sur les chambres du crud", () => {
     })
  */
 
-    it('chambre doit supprimer la nouvelle chambre et répondre avec 333', () => {
-        ChambreModels.delete(data);
-         
-        expect(ChambreModels.index('333')).toBeNull()
+        it('chambre doit supprimer la nouvelle chambre et répondre avec 333', () => {
+            ChambreModels.delete(data);
+            
+            expect(ChambreModels.index('333')).toBeNull()
 
-    })
+        })
 
 })
